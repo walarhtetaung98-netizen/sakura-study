b464dd4825614e06136a286d4f265a307566308f
walarhtetaung98-netizen <walarhtetaung98@gmail.com>
2026-01-30 16:00:24 +0630

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Header } from './components/Header';
+import { Header } from './components/Header.tsx';
 import { Dashboard } from './components/Dashboard';
 import { QuizCard } from './components/QuizCard';
 import { StudyCard } from './components/StudyCard';
